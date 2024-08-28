f01b48bf7d5da35064fd941656e75c74baf920d2
JulyAnira <146862254+JulyAnira@users.noreply.github.com>
2024-08-28 14:24:21 -0500

    p5jscolor

---
+++ sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(windowWidth,windowHeight);
+  }
+  
+  function draw() {
+    background(123,43,23);
+    rect(mouseX,mouseY,mouseX,mouseY);
+    fill(255, 204, 0);
+  }
