d26fc372c19380f4074e002a1c6a9fa21e903f35
JulyAnira <146862254+JulyAnira@users.noreply.github.com>
2024-09-02 00:36:32 -0500

    change the background color to red--black

--- sketch.js
+++ sketch.js
@@ -1,13 +1,23 @@
 function setup() {
     createCanvas(windowWidth,windowHeight);
   }
-  
+  function windowResized(){
+    resizeCanvas(windowWidth,windowHeight);
+    background(123,43,23);
+  }  
   function draw() {
     background(123,43,23);
     rect(mouseX,mouseY,mouseX,mouseY);
     fill(255, 204, 0);
+
+    //color
+    let startColor = color(204, 0, 0);
+    let endColor = color(32, 32, 32);
+  
+  for (let y=0; y<height; y++){
+    let interA = map(y, 0, height, 0, 1);
+    let c = lerpColor(startColor, endColor, interA);
+    stroke(c);
+    line(0, y, width, y);
   }
-  function windowResized(){
-    resizeCanvas(windowWidth,windowHeight);
-    background(123,43,23);
-  }
+}
