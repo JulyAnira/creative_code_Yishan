fd93eec291ce3a1bba2fde5036d7bf2f20d7d0f4
JulyAnira <146862254+JulyAnira@users.noreply.github.com>
2024-09-02 22:38:37 -0500

    background to pink

--- sketch.js
+++ sketch.js
@@ -6,18 +6,38 @@ function setup() {
     background(123,43,23);
   }  
   function draw() {
-    background(123,43,23);
-    rect(mouseX,mouseY,mouseX,mouseY);
-    fill(255, 204, 0);
+  let colors = [
+    color(240, 174, 174),
+    color(246, 105, 124),
+    color(198, 43, 79),
+  ] 
+  setBackground(colors);
 
-    //color
-    let startColor = color(204, 0, 0);
-    let endColor = color(32, 32, 32);
-  
-  for (let y=0; y<height; y++){
-    let interA = map(y, 0, height, 0, 1);
-    let c = lerpColor(startColor, endColor, interA);
-    stroke(c);
-    line(0, y, width, y);
+    function setBackground(colors){
+      for (let y = 0; y < height; y++) {
+        //ps: colors.length - 1 is the last number of array
+        /* 通过map将y（行数）的范围‘0, height’映射到‘colors.length - 1’
+        这个值通常为小数
+        */
+        let interColorIndex = map(y, 0, height, 0, colors.length - 1);
+        let colorIndex = floor(interColorIndex);
+        /* floor() 函数将 interColorIndex 向下取整，
+        即舍去小数部分，得到一个整数索引 colorIndex。*/
+        let inter = interColorIndex - colorIndex;
+        /*inter 是一个在 [0, 1] 范围内的小数，
+        用于表示当前行颜色在两个相邻颜色之间的渐变程度*/
+        
+
+        //Used to define the color transition range for the current row
+        let startColor = colors[colorIndex];
+        /* endColor is the next color, and 
+        never exceed the last index of the array */
+        let endColor = colors[min(colorIndex + 1, colors.length - 1)];
+        let c = lerpColor(startColor, endColor, inter);
+
+        stroke(c);
+        line(0, y, width, y);
+
+    }
   }
 }
