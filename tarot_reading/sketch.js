63677e84438b94de577d35ce5132023340b21e40
JulyAnira <146862254+JulyAnira@users.noreply.github.com>
2024-10-16 15:20:16 -0500

    Update sketch.js

--- tarot_reading/sketch.js
+++ tarot_reading/sketch.js
@@ -553,7 +553,7 @@ function gotResult(results) {
       imageFound = false;
       label = "none";
       state = "close";
-    }, 9000);
+    }, 2000);
   }
   let confidence = "Confidence: " + nf(results[0].confidence, 0, 2);
 }
